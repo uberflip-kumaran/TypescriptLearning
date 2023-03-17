68be2d3aaa4ae7c23ac5b98d32dd3deb43025567
shareeza <shareeza.hussain@uberflip.com>
2023-03-17 17:18:29 -0400

    set up env file for vite

--- movie-db/src/useFetch.tsx
+++ movie-db/src/useFetch.tsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import ('dotenv');
+
+
 //const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
-const API_ENDPOINT = `https://www.omdbapi.com/?apikey=388f5944`;
+const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_APP_MOVIE_API_KEY}`;
+
+console.log(API_ENDPOINT);
 
 const useFetch = (urlParams:string) => {
   const [isLoading, setIsLoading] = useState(true)
