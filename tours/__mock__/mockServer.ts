586c7c9f58d24df1e124a47c6da30c74e308d28e
Shuo Chen <shuochen@Shuo.Chen>
2023-04-06 11:29:19 -0700

    move mock server to a new file.

---
+++ tours/__mock__/mockServer.ts
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import { setupServer } from 'msw/node';
+// import { rest } from 'msw';
+
+// import { mockTours } from './sampleTours';
+
+
+// const handlers = [
+//   rest.get('http://course-api.com/react-tours-project', (req, res, ctx) => {
+//     return res(ctx.json(mockTours));
+//   }),
+// ];
+
+// export const server = setupServer(...handlers);
